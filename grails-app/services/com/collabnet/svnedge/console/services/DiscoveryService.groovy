ac97b79b9a3287b14dee20939eeff91760ad04f0
Marcello de Sales <marcello.desales@gmail.com>
2011-03-02 00:16:19 +0000

    [artf5534] This revision removes old controllers used by the replica prototype, as well as old packages and tests.
    * MasterController
    * ReplicaController
    * UserCacheController
    - Low-level visualization of the user's auth/authz cache still in use by the
    replica, but not displayed to the admin.
    * WizardController
    - Removing controllers used by the prototype not used.
    
    * RegistrationService.groovy
    - Removing the old replica registration service.
    
    * Master.groovy
    * ReplicaConfig.groovy
    - Domain classes that were part of the prototype. Today it is implemeted by
    CtfServer.groovy and ReplicaConfiguraiton.groovy domain classes.
    
    * src/groovy/com/collabnet/svnedge/ws
    - package not used.
    
    * *Tests.groovy
    - Removing test cases.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/svnedge/trunk@1836 03e8f217-bfc6-4b7c-bcb7-0738c91e2c5f

--- grails-app/services/com/collabnet/svnedge/console/services/DiscoveryService.groovy
+++ grails-app/services/com/collabnet/svnedge/console/services/DiscoveryService.groovy
@@ -93,6 +93,7 @@ class DiscoveryService {
             def params = [:]
             params[SvnEdgeCsvnServiceKey.TEAMFORGE_PATH] = tfPath
             params[SvnEdgeCsvnServiceKey.CONTEXT_PATH] = path
+            params[SvnEdgeCsvnServiceKey.SERVER_MODE] = server.mode
 
             register.registerService(port, SvnEdgeServiceType.CSVN, params);
 
