3d3e5b822a826ffcd343f9af22b2c201b8f8464c
Marcello de Sales <marcello.desales@gmail.com>
2011-03-02 00:25:51 +0000

    [artf5534] This revision moves controllers to appropriate packages: console and teamforge.
    * LoginController.groovy
    * LogoutController.groovy
    - Makes part of the console.
    
    * SetupReplicaControoler.groovy
    * SetupTeamForgeController.groovy
    - Makes part of the teamforge package.
    
    * ConversionController.groovy
    - Removing the old conversion controller.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/svnedge/trunk@1837 03e8f217-bfc6-4b7c-bcb7-0738c91e2c5f

--- grails-app/controllers/com/collabnet/svnedge/console/ui/LogoutController.groovy
+++ grails-app/controllers/com/collabnet/svnedge/console/ui/LogoutController.groovy
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.collabnet.svnedge.replica.auth
+package com.collabnet.svnedge.console.ui
+
 
 /**
  * Logout Controller (Example).
--- grails-app/controllers/com/collabnet/svnedge/teamforge/SetupReplicaController.groovy
+++ grails-app/controllers/com/collabnet/svnedge/teamforge/SetupReplicaController.groovy
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.collabnet.svnedge.console.ui
+package com.collabnet.svnedge.teamforge
+
 
 import org.codehaus.groovy.grails.plugins.springsecurity.Secured
 
