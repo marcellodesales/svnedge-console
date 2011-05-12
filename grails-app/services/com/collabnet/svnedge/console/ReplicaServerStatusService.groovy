30a1e46fe8ede6d01b4ae743bc9155e843b3841a
Marcello de Sales <marcello.desales@gmail.com>
2011-05-12 17:10:57 +0000

    [artf5552] This revision adds fixes the previous revision that had the uncommitted class. The problem was that the class was refactored and the service does not contain methods that were depending on the missing classes.
    * ReplicaServerStatusService.groovy
    - Removing all related to the command execution. 
    - This is the initial implementation of the channel service for the 
    UI activity. Not yet in bootstrap or used.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/svnedge/trunk@2046 03e8f217-bfc6-4b7c-bcb7-0738c91e2c5f

--- grails-app/services/com/collabnet/svnedge/console/ReplicaServerStatusService.groovy
+++ grails-app/services/com/collabnet/svnedge/console/ReplicaServerStatusService.groovy
@@ -1,7 +1,6 @@
 package com.collabnet.svnedge.console
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import grails.converters.JSON;
 
@@ -9,7 +8,7 @@ import org.cometd.Client;
 import org.mortbay.cometd.ChannelImpl;
 import org.springframework.beans.factory.InitializingBean;
 
-import com.collabnet.svnedge.util.RealTimeCommandLineListener;
+import com.collabnet.svnedge.util.CommandLineOutputListener;
 import static com.collabnet.svnedge.console.CommandLineService.COMMAND_TERMINATED;
 
 public final class ReplicaServerStatusService implements InitializingBean {
@@ -46,16 +45,6 @@ public final class ReplicaServerStatusService implements InitializingBean {
     }
 
     /**
-     * Non-blocking command executor with this instance with the instance
-     * of this service.
-     * @param command command to be executed.
-     */
-    def executeCommand(command) {
-        def bool = this instanceof RealTimeCommandLineListener
-        commandLineService.executeWithCommandLineListener(command, this)
-    }
-
-    /**
      * An asynchronous publisher that publishes the given message in the 
      * Bayeux server with the following Json doc:
      * 
@@ -80,32 +69,6 @@ public final class ReplicaServerStatusService implements InitializingBean {
     void receiveCommandLineOutputLine(String ommandOutputLine) {
         if (!commandOutputLine.equals(COMMAND_TERMINATED)) {
             publishPercentageMessage(commandOutputLine)
-        }
-    }
-
-    /**
-     * Executes a command in the background and returns a listener reference to be
-     * @param command
-     * @return
-     */
-    public RealTimeCommandLineListener execute(String command) {
-        def outputQueue = new LinkedBlockingQueue<String>()
-        def listener = new RealTimeCommandLineListener(outputQueue)
-        commandLineService.executeWithCommandLineListener(command, outputQueue)
-        return listener
-    }
-
-    public static void main(String[] args) {
-        def cmd = "ping www.google.com -c 5"
-
-        def realTimeCmd = new ReplicaServerStatusService()
-        realTimeCmd.commandLineService = new CommandLineService()
-        def cmdListener = realTimeCmd.execute(cmd)
-
-        println "The output... ###########################"
-        def line
-        while ((line = cmdListener.getNextOutputLine()) != null) {
-             println line
         }
     }
 }
