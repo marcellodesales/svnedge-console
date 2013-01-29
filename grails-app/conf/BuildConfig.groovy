96a652d08bd2fcdda671cdb3b4c71b50c8061cb8
John Mcnally <jmcnally@collab.net>
2013-01-29 19:56:20 +0000

    [artf7708] Remove unused jars
    * grails-app/controllers/com/collabnet/svnedge/controller/LoginController.groovy
      Removing openId, facebook, and rememberMe related code as it is not used.
    
    * grails-app/conf/BuildConfig.groovy
      Excluding default jsp libraries and removing other unused jars during staging
      of the war. 
    
    * svn-server/appserver/lib/jndi/mail-1.4.jar
    * svn-server/appserver/lib/jndi/activation-1.1.jar
    * svn-server/appserver/lib/jsp/jsp-2.1-glassfish-2.1.v20091210.jar
    * svn-server/appserver/lib/jsp/jsp-api-2.1-glassfish-2.1.v20091210.jar
    * svn-server/appserver/lib/jsp/core-3.1.1.jar
    * svn-server/appserver/lib/jsp/jetty-jsp-2.1-7.0.2.v20100331.jar
    * svn-server/appserver/lib/jsp/ant-1.6.5.jar
      Removing mail and jsp related dependencies. In the case of mail, a plugin
      contains a newer version.
    
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/svnedge/trunk@3422 03e8f217-bfc6-4b7c-bcb7-0738c91e2c5f

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,6 +8,15 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
+		//excludes group: 'org.mortbay.jetty', name: 'jsp-2.0'
+        excludes group: 'tomcat', name:'jasper-compiler'
+        excludes group: 'tomcat', name:'jasper-compiler-jdt'
+        excludes group: 'tomcat', name:'jasper-runtime'
+        excludes group: 'org.eclipse.jdt', name: 'core'
+        excludes group: 'javax.servlet', name: 'jstl'
+        excludes group: 'apache-taglibs', name: 'standard'
+        excludes 'commons-el'
+        excludes group: 'net.sourceforge.nekohtml', name: 'nekohtml'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
@@ -30,3 +39,46 @@ grails.project.dependency.resolution = {
         compile 'org.ini4j:ini4j:0.5.2'
     }
 }
+grails.war.resources = { stagingDir ->
+    def libDir = new File(stagingDir, 'WEB-INF/lib')
+    def deleteJars = { name, excludeList = [] ->
+        libDir.eachFile { f ->
+           if (f.name.startsWith(name)) {
+               boolean isDelete = true
+               excludeList?.each {
+                   if (f.name.startsWith(it)) {
+                       isDelete = false
+                   }
+               }
+               if (isDelete) {
+                   f.delete()
+                   println "Deleted ${f.name}"
+               }
+           }
+        }
+    }
+    deleteJars("CodeNarc")
+    deleteJars("FastInfoset")
+    // acegi plugin includes several auth mechanisms we don't use
+    deleteJars("cas-client-core")
+    deleteJars("ant-contrib")
+    deleteJars("facebook-java-api")
+    deleteJars("easymock")
+    deleteJars("htmlparser")
+    deleteJars("jcifs")
+    deleteJars("json-20070829")
+    deleteJars("openid4java")
+    deleteJars("openxri-client")
+    deleteJars("openxri-syntax")    
+    deleteJars("spring-ldap")
+    deleteJars("spring-security-cas-client")
+    deleteJars("spring-security-openid")
+    deleteJars("xmlsec")
+    //deleteJar("")
+    // jetty plugin requires some default globally inherited jars to build, but are
+    // not needed in the war.  Should be possible to configure this better, but
+    // nothing I've tried in "dependencies" or "plugins" allows the plugin to build.
+    // And we also invoke jetty code in 'run-app' mode to deploy integration war.
+    deleteJars("jetty", ['jetty-util'])
+}
+  
